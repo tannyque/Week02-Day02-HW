b57ed585f206c500c3f3a82a470ed60b2d185642
tannyque <tannyque1992@gmail.com>
2018-07-25 09:44:45 +0100

    Removes setup and adds fish in the testing

--- specs/fish_spec.rb
+++ specs/fish_spec.rb
@@ -1,19 +1,18 @@
 require("minitest/autorun")
 require("minitest/rg")
-require_relative("../river")
-require_relative("../bears")
 require_relative("../fish")
 
 class FishTest < MiniTest::Test
 
-  def setup()
-    @fish1 = Fish.new("Nemo")
-    @fish2 = Fish.new("Marlin")
-    @fish3 = Fish.new("Dory")
-  end
+  # def setup()
+  #   @fish1 = Fish.new("Nemo")
+  #   @fish2 = Fish.new("Marlin")
+  #   @fish3 = Fish.new("Dory")
+  # end
 
   def test_fish_name
-    assert_equal("Nemo", @fish1.name())
+    fish = Fish.new("Nemo")
+    assert_equal("Nemo", fish.name())
   end
 
 end
